af91dc95f45d8d506728e645cbbea344f5f216a4
Nosferatu783 <smith.infosec@gmail.com>
2024-11-06 19:08:50 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Determine API base URL dynamically
-const API_BASE_URL = `https://to-do-oxeu.onrender.com`;
+const API_BASE_URL = `https://to-do-oxeu.onrender.com/api`;
 
 // Select elements
 const registerForm = document.getElementById('register-form');
